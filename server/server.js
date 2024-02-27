5191fbd0544f4f182e140f915eef621735578430
RadiazOm <jeffrey.otterloo@gmail.com>
2024-02-27 18:34:25 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+// express
+
+// connectie met OpenAI (Azure)
+
+import { ChatOpenAI } from "@langchain/openai"
+
+const model = new ChatOpenAI({
+    azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
+    azureOpenAIApiVersion: process.env.OPENAI_API_VERSION,
+    azureOpenAIApiInstanceName: process.env.INSTANCE_NAME,
+    azureOpenAIApiDeploymentName: process.env.ENGINE_NAME,
+})
+
+const joke = await model.invoke("pls help i am stuck grandchamp 1 in champion2 how do i get better")
+console.log(joke.content)
